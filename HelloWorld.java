59e7f7fbf4db8c46db2f08ecb4eda4220da9da99
SAMSINGH-coder <68236559+SAMSINGH-coder@users.noreply.github.com>
2024-04-30 16:04:54 +0530

    Update HelloWorld.java - modified print statement

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-System.out.println("Hello World!, This is my first jenkins program");
+System.out.println("Hello World!");
     }
 }
